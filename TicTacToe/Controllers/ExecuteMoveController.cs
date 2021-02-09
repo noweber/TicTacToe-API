f4f2d49954634ab2bf429e86a258a0687a259c5f
Nicholas Owen Weber <noweber@gmail.com>
2021-02-09 15:56:25 -0800

    Execute Move Request and Response Models

--- TicTacToe/Controllers/ExecuteMoveController.cs
+++ TicTacToe/Controllers/ExecuteMoveController.cs
@@ -4,12 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicTacToe.DataTransferObjects;
 
 namespace TicTacToe.Controllers
 {
+    /// <summary>
+    /// A REST controller to provide an interface for executing moves within the Tic Tac Toe game.
+    /// </summary>
     [Route("[controller]")]
     [ApiController]
     public class ExecuteMoveController : ControllerBase
     {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns>TODO</returns>
+        [HttpPost]
+        public IActionResult Post(Move moveRequest)
+        {
+            return null;
+        }
     }
 }
---
+++ TicTacToe/DataTransferObjects/ExecuteMoveResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.DataTransferObjects
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class ExecuteMoveResponse : Move
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public string winner { get; set; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public string winPositions { get; set; }
+    }
+}
