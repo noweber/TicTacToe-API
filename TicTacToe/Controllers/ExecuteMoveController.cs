e28a44a9fa2d71dd4a9881602168ab59f5b784a1
Nicholas Owen Weber <noweber@gmail.com>
2021-02-09 22:09:44 -0800

    Validate Azure is the Next Player to Move in ExecuteMove

--- TicTacToe/Controllers/ExecuteMoveController.cs
+++ TicTacToe/Controllers/ExecuteMoveController.cs
@@ -25,12 +25,15 @@ namespace TicTacToe.Controllers
         public IActionResult Post(ExecuteMoveRequest moveRequest)
         {
             // Check that the Azure and Human player symbols are different:
+            // If the symbols in the request body are the same, return Bad Request.
             if (string.Equals(moveRequest.azurePlayerSymbol, moveRequest.humanPlayerSymbol))
             {
                 return BadRequest("The Azure and Human players must use different symbols. Only X and O are permissable.");
             }
 
             // Check that the game board is valid:
+            // If there are invalid symbols in the game board, return Bad Request.
+            // This also counts the number of Xs and Os present in the game board for further checks.
             int countOfXMoves = 0;
             int countOfOMoves = 0;
             for (int i = 0; i < moveRequest.gameBoard.Length; i++)
@@ -50,7 +53,8 @@ namespace TicTacToe.Controllers
             }
 
             // Check that the number of X and O moves are valid:
-            if(Math.Abs(countOfXMoves - countOfOMoves) > 1)
+            // If there is +/- 1 of one symbol relative to the other, return Bad Request.
+            if (Math.Abs(countOfXMoves - countOfOMoves) > 1)
             {
                 return BadRequest("The game board contains an illegal move. There must be an equal number of X and O moves within the board +/- 1 for the most recent move.");
             }
@@ -61,6 +65,11 @@ namespace TicTacToe.Controllers
                 return BadRequest("The move does not match the game board state. The symbol on the game board at the move's index must match the human player's symbol.");
             }
 
+            // Check that Azure is the next actor to make a turn given the current state of the board:
+            if (!string.Equals(this.NextPlayer(moveRequest.gameBoard, moveRequest.azurePlayerSymbol), moveRequest.azurePlayerSymbol))
+            {
+                return BadRequest("The game board is invalid because the Azure player is not the next player to move and therefore cannot execute a valid move from the current board state.");
+            }
 
             return new ObjectResult(new ExecuteMoveResponse()
             {
@@ -71,6 +80,42 @@ namespace TicTacToe.Controllers
                 winner = null,
                 winPositions = null
             });
+        }
+
+        /// <summary>
+        /// A function to determine the next player given the current state of the Tic Tac Toe board.
+        /// </summary>
+        /// <param name="board">The string array representing the nine cells in a Tic Tac Toe game board.</param>
+        /// <param name="tieBreakerSymbol">In the result of a tie between the number of Xs and Os, this symbol will go first.</param>
+        /// <returns>Returns X if there more Os than Xs, returns O if there are more Xs than Os, or returns the tieBreakerSymbol input if the number of Xs equals the number of Os.</returns>
+        private string NextPlayer(string[] board, string tieBreakerSymbol)
+        {
+            int xCount = 0;
+            int OCount = 0;
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (string.Equals(board[i], "X"))
+                {
+                    xCount += 1;
+                }
+                else if (string.Equals(board[i], "O"))
+                {
+                    OCount += 1;
+                }
+            }
+
+            if (xCount > OCount)
+            {
+                return "O";
+            }
+            else if (OCount > xCount)
+            {
+                return "X";
+            }
+            else
+            {
+                return tieBreakerSymbol;
+            }
         }
     }
 }
