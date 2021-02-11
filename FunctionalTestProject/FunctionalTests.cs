cd49d7eccaddf65326c9cd80c5c39796efffbda4
Nicholas Owen Weber <noweber@gmail.com>
2021-02-11 07:17:47 -0800

    Created Functional Tests for Handing of Invalid Execute Move Data

--- FunctionalTestProject/FunctionalTests.cs
+++ FunctionalTestProject/FunctionalTests.cs
@@ -246,6 +246,8 @@ namespace FunctionalTestProject
             Assert.AreEqual("The Azure and Human players must use different symbols. Only X and O are permissable.", results);
         }
 
+        #region Verify proper handling of invalid data
+
         /// <summary>
         /// This is a functional test to assert a BadRequest is returned when both players are said to use the O symbol.
         /// </summary>
@@ -273,6 +275,224 @@ namespace FunctionalTestProject
             // Assert the BadRequest description string is correct:
             Assert.IsTrue(response.Contains("The field humanPlayerSymbol must match the regular expression 'X|O'"));
         }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when both players are said to use the O symbol.
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenInvalidCharacterIsInTheGameBoard()
+        {
+            // Arrange a game board which contains an invalid symbol
+            string[] GameBoard = new string[]
+            {
+                X, _, "Y",
+                _, O, _,
+                _, _, _
+            };
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(X, O, GameBoard, 4);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The game board contains an invalid character. Only X, O, and ? are permissable."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when the game board contains an invalid number of moves by one player -> three Xs and only 1 O
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenTheGameBoardContainsInvalidNumberOfMovesByAPlayer()
+        {
+            // Arrange a game board which contains an invalid symbol
+            string[] GameBoard = new string[]
+            {
+                X, X, X,
+                _, O, _,
+                _, _, _
+            };
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(O, X, GameBoard, 4);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The game board contains an illegal move. There must be an equal number of X and O moves within the board +/- 1 for the most recent move."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when the game board contains fewer than 9 elements in its array
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenTheGameBoardContainsTooFewElements()
+        {
+            // Arrange a game board which contains too few elements (less than 9)
+            string[] GameBoard = new string[]
+            {
+                _, _, _,
+                _, _, _
+            };
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(O, X, GameBoard);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The field gameBoard must be a string or array type with a minimum length of '9'."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when the game board contains more than 9 elements in its array
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenTheGameBoardContainsTooManyElements()
+        {
+            // Arrange a game board which contains too many elements (more than 9)
+            string[] GameBoard = new string[]
+            {
+                _, _, _,
+                _, _, _,
+                _, _, _,
+                _, _, _
+            };
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(O, X, GameBoard);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The field gameBoard must be a string or array type with a maximum length of '9'."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when the move made by the player in the request is actually the other player's symbol on the game board sent.
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenPlayerMoveIsNotTheirSymbol()
+        {
+            // Arrange a game board with both player's symbols
+            string[] GameBoard = new string[]
+            {
+                X, _, _,
+                O, O, _,
+                X, _, _
+            };
+
+            // Arrange a request where the 'move' field corresponds to the other player's symbol on the game board.
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(O, X, GameBoard, 3);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The move does not match the game board state. The symbol on the game board at the move's index must match the human player's symbol."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when the move made by the player in the request's move field is not reflect in the game board sent.
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenPlayerMoveIsNotPresentInGameBoard()
+        {
+            // Arrange a game board with both player's symbols
+            string[] GameBoard = new string[]
+            {
+                X, _, _,
+                _, O, _,
+                X, _, _
+            };
+
+            // Arrange a request where the 'move' field corresponds to a "?" symbol on the game board.
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(X, O, GameBoard, 3);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The move does not match the game board state. The symbol on the game board at the move's index must match the human player's symbol."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when the game board is not empty, but the player did not send their next move in the move field.
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenPlayerMoveIsNotPresentInRequestButGameBoardIsNotEmpty()
+        {
+            // Arrange a non-empty game board
+            string[] GameBoard = new string[]
+            {
+                X, _, _,
+                _, O, _,
+                X, _, _
+            };
+
+            // Arrange a request with no 'move' value
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(X, O, GameBoard);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The move field does not contain a value, but the board is not empty."));
+        }
+
+        /// <summary>
+        /// This is a functional test to assert a BadRequest is returned when Azure should not be the next player to move given how many moves each player has made -- human has fewer symbols on the board, but has also just moved.
+        /// </summary>
+        [TestMethod]
+        public void TestPostExecuteMoveReturnsBadRequestWhenAzureHasMadeMoreMovesThanThePlayerInTheGameBoardSent()
+        {
+            // Arrange a game board where the human player has fewer moves made than Azure
+            string[] GameBoard = new string[]
+            {
+                X, O, X,
+                _, O, _,
+                X, _, _
+            };
+
+            // Arrange a request where the human has just moved, but according to the game board it should still be the human's move
+            RestClientSdkLibraryClient client = this.GetRestSdkClient();
+            ExecuteMoveRequest moveRequest = new ExecuteMoveRequest(X, O, GameBoard, 4);
+
+            // Act
+            string response = (string)client.Post(moveRequest);
+
+            // Assert the response object is not null to prove it was a BadRequest:
+            Assert.IsNotNull(response);
+
+            // Assert the BadRequest description string is correct:
+            Assert.IsTrue(response.Contains("The game board is invalid because the Azure player is not the next player to move and therefore cannot execute a valid move from the current board state."));
+        }
+
+        #endregion
 
         /// <summary>
         /// This is a helper function used by all of the functional tests to instnatiate the REST client object.
